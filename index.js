4e3b0d589049da943ebab564f507425a33605c0b
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2021-05-23 13:57:39 +0300

    Create index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ if(command == "kick") {
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!kUser) return message.channel.send("Kick failed, please **@mention** your target.");
         let kReason = args.join(" ").slice(0);
-        if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+        if(!message.member.hasPermission('ADMINISTRATOR')
             return message.reply("you are not permitted to use this.");
     
         let kickEmbed = new Discord.RichEmbed()
@@ -118,7 +118,7 @@ if(command == "kick") {
 //Admin commands.
 if(command === "admin") {
        message.delete().catch(O_o=>{});
-          if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+          if(!message.member.hasPermission('ADMINISTRATOR')
             return message.reply("you are not permitted to use this.");
               let botembed = new Discord.RichEmbed()
           .setDescription(`💬 __**dbo9h server admin commands**__\nYou must have the [*G_D_A*] role to be able to use these commands.`)
@@ -133,7 +133,7 @@ if(command === "admin") {
           .addField(`${config.prefix}` + "addtodo", "Adds a todo-entry to <#335008973126107139>.")
           .addField(`${config.prefix}` + "addvote", "Adds a vote-entry to <#335008891450687491>.")
           .addField(`${config.prefix}` + "addjob", "Adds a new listing to <#421330450418499605>.")
-          .addField(`${config.prefix}` + "dbo9htsupdate", "Adds a update-entry for dbo9h in the channel command was used.")
+          .addField(`${config.prefix}` + "dbo9hTSupdate", "Adds a update-entry for dbo9h in the channel command was used.")
           .addField(`${config.prefix}` + "say", "Let the bot do the talking.")
           .addField(`${config.prefix}` + "servers", "Show all servers bot is connected to.")
           .addField(`${config.prefix}` + "kick", "Kicks user specified with reason specified.")
@@ -145,7 +145,7 @@ if(command === "admin") {
 //Ping command. Regular and bot-to-websocket.  
   if(command === "ping") {
      message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
 
     const m = await message.channel.send("Ping?");
@@ -176,7 +176,7 @@ if(command === "admin") {
 
   //Say command. 
   if(command === "say") {
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
@@ -207,7 +207,7 @@ if(command === "help") {
 //Add into the joblist channel
 if (command === 'addjob') {
   message.delete().catch(O_o=>{});
-if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+if(!message.member.hasPermission('ADMINISTRATOR')
   return message.reply("you are not permitted to use this.");
   let str = `${args.join(' ')}` ;
   let Title = str.split(/"/)[1];
@@ -236,7 +236,7 @@ if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
 //Add into the faq channel
 if (command === 'addfaq') {
   message.delete().catch(O_o=>{});
-if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+if(!message.member.hasPermission('ADMINISTRATOR')
   return message.reply("you are not permitted to use this.");
   let str = `${args.join(' ')}` ;
   let Question = str.split(/"/)[1];
@@ -265,7 +265,7 @@ if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
 //Add into the todolist channel
 if (command === 'addtodo') {
   message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
   return message.reply("you are not permitted to use this.");
   let str = `${args.join(' ')}` ;
   let TodoThing = str.split(/"/)[1];
@@ -291,7 +291,7 @@ if (command === 'addtodo') {
 //Voting system
 if (command === 'addvote') {
   message.delete().catch(O_o=>{});
-  if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+  if(!message.member.hasPermission('ADMINISTRATOR')
   return message.reply("you are not permitted to use this.");
   let str = `${args.join(' ')}` ;
   let VoteThing = str.split(/"/)[1];
@@ -320,7 +320,7 @@ if (command === 'addvote') {
 // Command used for announcing Gaia Wyverns updates.
 // if(command === "gwupdate") {
 //       message.delete().catch(O_o=>{});
-//  if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+//  if(!message.member.hasPermission('ADMINISTRATOR')
 //       return message.reply("you are not permitted to use this.");
 //       message.channel.send("@everyone");
 //       let botembed = new Discord.RichEmbed()
@@ -356,7 +356,7 @@ if (command === 'addvote') {
 //Command used for announcing Dino Hunter updates.
 //if(command === "dhupdate") {
 //      message.delete().catch(O_o=>{});
-// if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+// if(!message.member.hasPermission('ADMINISTRATOR')
 //      return message.reply("you are not permitted to use this.");
 //      message.channel.send("@everyone");
 //      let botembed = new Discord.RichEmbed()
@@ -390,9 +390,9 @@ if (command === 'addvote') {
 //          }
 //
 //Command used for announcing dbo9h updates.
-if(command === "dbo9htsupdate") {
+if(command === "gaiaupdate") {
       message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
       message.channel.send("@everyone");
       let botembed = new Discord.RichEmbed()
@@ -428,7 +428,7 @@ if(command === "dbo9htsupdate") {
 ////Command used for announcing Gaia updates.
 //if(command === "gaiabeaconsupdate") {
 //      message.delete().catch(O_o=>{});
-// if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+// if(!message.member.hasPermission('ADMINISTRATOR')
 //      return message.reply("you are not permitted to use this.");
 //      message.channel.send("@everyone");
 //      let botembed = new Discord.RichEmbed()
@@ -464,7 +464,7 @@ if(command === "dbo9htsupdate") {
 //Show testserver info
 if(command === "testserver") {
     message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
       let string = '';
  {
@@ -483,7 +483,7 @@ if(command === "testserver") {
 //Servers command
   if (command === "servers") {
   message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
     let string = '';
 
@@ -503,7 +503,7 @@ if(command === "testserver") {
 //Google.
   if(command === "google") {
       message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
       message.channel.send(`The answer to your question can be found here <http://tiny.cc/bd8gfz>.`);
   }
@@ -511,7 +511,7 @@ if(command === "testserver") {
 //Uptime command. Specified in milliseconds. % is modulo.
   if(command === "uptime") {
       message.delete().catch(O_o=>{});
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
       let days = Math.floor(client.uptime / 86400000);
       let hours = Math.floor(client.uptime / 3600000) % 24;
@@ -523,7 +523,7 @@ if(command === "testserver") {
 
 //Purge command. 
 if(command === "purge") {
- if(!message.member.roles.some(r=>["G_D_A"].includes(r.name)) )
+ if(!message.member.hasPermission('ADMINISTRATOR')
       return message.reply("you are not permitted to use this.");
     const deleteCount = parseInt(args[0], 10);
         if(!deleteCount || deleteCount < 2 || deleteCount > 100)
