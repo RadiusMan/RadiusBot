9d8c7fd4a58215cc456f3cc5c80cc356d0775876
RadiusMan <84643804+RadiusMan@users.noreply.github.com>
2021-05-23 18:43:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const GaiaOfficialRole = '594967855473360926';
 
 
-const { ReactionRoleManager } = require('discord.js-collector'); //We import the discord.js-collector package that'll make reaction roles possible
 
 
 //Set script as client-bot.
@@ -17,52 +16,9 @@ const config = require("./config.json");
 //Define owner. Me.
 const ownerID = '537320927378014211';
 
-const reactionRoleManager = new ReactionRoleManager(client, {
- //We create a reaction role manager that'll handle everything related to reaction roles
- storage: true, // Enable reaction role store in a Json file
- path: __dirname + '/roles.json', // Where will save the roles if store is enabled
- mongoDbLink: 'url mongoose link', // See here to see how setup mongoose: https://github.com/IDjinn/Discord.js-Collector/tree/dev/examples/reaction-role-manager/Note.md & https://medium.com/@LondonAppBrewery/how-to-download-install-mongodb-on-windows-4ee4b3493514
-});
-
 client.on('ready', () => {
  console.log('ready');
 });
-
-
-//testtttttttttttttttt
-client.on('message', async (message) => {
- const client = message.client;
- const args = message.content.split(' ').slice(1);
- // Example
- // >createReactionRole @role :emoji: MessageId
- if (message.content.startsWith('>createReactionRole')) {
-  const role = message.mentions.roles.first();
-  if (!role)
-   return message
-    .reply('You need mention a role')
-    .then((m) => m.delete({ timeout: 1_000 }));
-
-  const emoji = args[1];
-  if (!emoji)
-   return message
-    .reply('You need use a valid emoji.')
-    .then((m) => m.delete({ timeout: 1_000 }));
-
-  const msg = await message.channel.messages.fetch(args[2] || message.id);
-  if (!role)
-   return message
-    .reply('Message not found!')
-    .then((m) => m.delete({ timeout: 1_000 }));
-
-  reactionRoleManager.addRole({
-   message: msg,
-   role,
-   emoji,
-  });
-  message.reply('Done').then((m) => m.delete({ timeout: 500 }));
- }
-});
-//////////////////
 
 
 //console.log('All users listed.');
